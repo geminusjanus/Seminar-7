1fe220ffa15255b50f70f756cfd7b14ac6e3ce72
Nikita <silvernikchuprov@gmail.com>
2022-10-28 14:43:49 +0300

    7

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -10,25 +10,25 @@ int stringArray = 3;
 int column = 4;
 int[,] intArray = new int[stringArray, column];
 Random rnd = new Random();
-void PrintArray(int[,] massive)
+void PrintArray(int[,] matrix)
 {
     for (int i = 0; i < stringArray; i++)
     {
         for (int j = 0; j < column; j++)
         {
-            Console.Write($"{massive[i, j]} ");
+            Console.Write($"{matrix[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 
-void FillArray(int[,] massive)
+void FillArray(int[,] matrix)
 {
     for (int i = 0; i < stringArray; i++)
     {
         for (int j = 0; j < column; j++)
         {
-            massive[i, j] = rnd.Next(0, 10);
+            matrix[i, j] = rnd.Next(0, 10);
         }
     }
 }
@@ -37,8 +37,8 @@ double AvrgElements(int [,] intArray)
 {
     double avrg = 0;
     int length = 0;
-    if intArray.GetLegth(0) > intArray.GetLength(1)
-    for (int i = 0; i < intArray.GetLength; i++)
+    if (intArray.GetLength(0) > intArray.GetLength(1))
+    for (int i = 0; i < intArray.Length; i++)
     {
         
     }
